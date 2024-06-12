84850d8966d1d5ec7c89c0863fb4fc33abd4a959
aosiot <cc@aosiot.com>
2024-06-12 16:35:35 +0800

    fixed

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,14 @@
 import { TokenProvider } from './token'
 import { Backend } from './backend'
+import HTML from './docker.html'
 
-const PROXY_HEADER_ALLOW_LIST: string[] = ["accept", "user-agent", "accept-encoding"]
+const PROXY_HEADER_ALLOW_LIST: string[] = [
+  'accept',
+  'user-agent',
+  'accept-encoding',
+]
 
-const validActionNames = new Set(["manifests", "blobs", "tags", "referrers"])
+const validActionNames = new Set(['manifests', 'blobs', 'tags', 'referrers'])
 
 // const ORG_NAME_BACKEND:{ [key: string]: string; } = {
 //   "gcr": "https://gcr.io",
@@ -11,15 +16,15 @@ const validActionNames = new Set(["manifests", "blobs", "tags", "referrers"])
 //   "quay": "https://quay.io",
 // }
 
-const DEFAULT_BACKEND_HOST: string = "https://registry-1.docker.io"
+const DEFAULT_BACKEND_HOST: string = 'https://registry-1.docker.io'
 
 export async function handleRequest(request: Request): Promise<Response> {
   return handleRegistryRequest(request)
 }
 
-function copyProxyHeaders(inputHeaders: Headers) : Headers {
-  const headers = new Headers;
-  for(const pair of inputHeaders.entries()) {
+function copyProxyHeaders(inputHeaders: Headers): Headers {
+  const headers = new Headers()
+  for (const pair of inputHeaders.entries()) {
     if (pair[0].toLowerCase() in PROXY_HEADER_ALLOW_LIST) {
       headers.append(pair[0], pair[1])
     }
@@ -27,7 +32,7 @@ function copyProxyHeaders(inputHeaders: Headers) : Headers {
   return headers
 }
 
-function orgNameFromPath(pathname: string): string|null {
+function orgNameFromPath(pathname: string): string | null {
   // const splitedPath: string[] = pathname.split("/", 3)
   // if (splitedPath.length === 3 && splitedPath[0] === "" && splitedPath[1] === "v2") {
   //   return splitedPath[2].toLowerCase()
@@ -35,7 +40,7 @@ function orgNameFromPath(pathname: string): string|null {
   return null
 }
 
-function hostByOrgName(orgName: string|null): string {
+function hostByOrgName(orgName: string | null): string {
   // if (orgName !== null && orgName in ORG_NAME_BACKEND) {
   //   return ORG_NAME_BACKEND[orgName]
   // }
@@ -43,15 +48,26 @@ function hostByOrgName(orgName: string|null): string {
 }
 
 function rewritePath(orgName: string | null, pathname: string): string {
-  let splitedPath = pathname.split("/");
+  let splitedPath = pathname.split('/')
 
   // /v2/repo/manifests/xxx -> /v2/library/repo/manifests/xxx
   // /v2/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
-  if (orgName === null && splitedPath.length === 5 && validActionNames.has(splitedPath[3])) {
-    splitedPath = [splitedPath[0], splitedPath[1], "library", splitedPath[2], splitedPath[3], splitedPath[4]]
+  if (
+    orgName === null &&
+    splitedPath.length === 5 &&
+    validActionNames.has(splitedPath[3])
+  ) {
+    splitedPath = [
+      splitedPath[0],
+      splitedPath[1],
+      'library',
+      splitedPath[2],
+      splitedPath[3],
+      splitedPath[4],
+    ]
   }
 
-  return splitedPath.join("/")
+  return splitedPath.join('/')
 
   // if (orgName === null || !(orgName in ORG_NAME_BACKEND)) {
   //   return pathname
@@ -65,11 +81,19 @@ function rewritePath(orgName: string | null, pathname: string): string {
 
 async function handleRegistryRequest(request: Request): Promise<Response> {
   const reqURL = new URL(request.url)
-  const orgName = orgNameFromPath(reqURL.pathname)
-  const pathname = rewritePath(orgName, reqURL.pathname)
-  const host = hostByOrgName(orgName)
-  const tokenProvider = new TokenProvider()
-  const backend = new Backend(host, tokenProvider)
-  const headers = copyProxyHeaders(request.headers)
-  return backend.proxy(pathname, {headers: request.headers})
+  if (reqURL.pathname === '/') {
+    return new Response(HTML, {
+      status: 200,
+      headers: {
+        'content-type': 'text/html',
+      },
+    })
+  } else {
+    const orgName = orgNameFromPath(reqURL.pathname)
+    const pathname = rewritePath(orgName, reqURL.pathname)
+    const host = hostByOrgName(orgName)
+    const tokenProvider = new TokenProvider()
+    const backend = new Backend(host, tokenProvider)
+    return backend.proxy(pathname, { headers: request.headers })
+  }
 }
